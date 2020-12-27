adf1d292b5bb276d34c267568470508f31e07665
Mikhail-nex <Mikhail.nex@mail.ru>
2020-12-27 21:58:57 +0300

    2 commit

--- app/src/main/java/com/example/taxi/Anim.java
+++ app/src/main/java/com/example/taxi/Anim.java
@@ -2,6 +2,7 @@ package com.example.taxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Anim extends AppCompatActivity {
@@ -10,5 +11,35 @@ public class Anim extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anim);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run () {
+                super.run();
+                {
+                    try{
+                   sleep(5000);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                        finally {
+                            Intent welcomeIntent = new Intent(Anim.this, Vhod.class);
+                            startActivity(welcomeIntent);
+                        }
+
+                }
+            }
+        };
+        thread.start();
     }
+
+    @Override
+    protected void onPause () {
+        super.onPause();
+
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/taxi/Vhod.java
+++ app/src/main/java/com/example/taxi/Vhod.java
@@ -2,13 +2,33 @@ package com.example.taxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Vhod extends AppCompatActivity {
+public class Vhod extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vhod);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick (View view) {
+            switch (view.getId()) {
+                case R.id.button:
+                    Intent intent = new Intent(this, Sign_up.class);
+                    startActivity(intent);
+                    break;
+                default:
+                    break;
+            }
     }
 }
